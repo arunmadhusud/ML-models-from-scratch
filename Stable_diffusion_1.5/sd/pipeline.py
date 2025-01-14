801aeca2162528f777a36bb660a0e5a011fac58c
arunmadhusud <arunsreekuttan1996@gmail.com>
2025-01-14 15:42:01 -0500

    Fixed bugs in img2img pipeline of stable diffusion

--- Stable_diffusion_1.5/sd/pipeline.py
+++ Stable_diffusion_1.5/sd/pipeline.py
@@ -83,6 +83,7 @@ def generate(
             input_image_tensor = input_image.resize((WIDTH, HEIGHT))
             input_image_tensor = np.array(input_image_tensor)
             input_image_tensor = torch.tensor(input_image_tensor, dtype=torch.float32)
+            input_image_tensor = input_image_tensor.to(device)
 
             input_image_tensor = rescale(input_image_tensor,(0,255),(-1,1)) 
 
